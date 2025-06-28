16cc73aef1a07c1d9b06fd88cdb4396eaaddb856
mitchnode <70460735+mitchnode@users.noreply.github.com>
2025-06-28 21:46:24 +1000

    Remove console.logs

--- src/dropdown.js
+++ src/dropdown.js
@@ -1,10 +1,7 @@
 export function dropdown() {
   const dropdown_button = document.getElementsByClassName("dropdown");
-  const dropdown_content = document.getElementsByClassName("dropdown_content");
-  const dropdown_contents = document.getElementById("dropdown_content");
-  console.log(dropdown_button.length);
+
   for (let n = 0; n < dropdown_button.length; n++) {
-    console.log(`Adding event listener to item ${n}`);
     dropdown_button.item(n).addEventListener("mouseover", (e) => {
       e.currentTarget
         .getElementsByClassName("dropdown_content")
