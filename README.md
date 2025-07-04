a755052b4b556866990ddc56737a0ddaae5b4975
mitchnode <70460735+mitchnode@users.noreply.github.com>
2025-07-04 10:50:50 +1000

    Add html to readme file. Move initial state into code of dropdown.js instead of css.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # dropdown
+
 Dropdown list module
+
+Create simple dropdown menus with a hover function
+
+Insert the following in your html
+
+<div class="dropdown">
+    <button class="dropdown_button">Menu</button>
+    <div class="dropdown_content">
+        <div class="dropdown_item">Edit</div>
+        <div class="dropdown_item">Copy</div>
+        <div class="dropdown_item">Delete</div>
+    </div>
+</div>
--- src/dropdown.js
+++ src/dropdown.js
@@ -1,6 +1,14 @@
 export function dropdown() {
   const dropdown_button = document.getElementsByClassName("dropdown");
 
+  // Initialize dropdown
+  const all_dropdown_content =
+    document.getElementsByClassName("dropdown_content");
+  for (let c = 0; c < all_dropdown_content.length; c++) {
+    all_dropdown_content.item(c).style.display = "none";
+  }
+
+  // Set up Event listeners for hover function
   for (let n = 0; n < dropdown_button.length; n++) {
     dropdown_button.item(n).addEventListener("mouseover", (e) => {
       e.currentTarget
